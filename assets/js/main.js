6a243aabe8e9a65409619c5f7ffbe79be3e3bcfe
Nathalya <nathalyperreira741@gmail.com>
2022-12-12 16:30:52 -0300

    projeto finalizado

---
+++ assets/js/main.js
@@ -0,0 +1,136 @@
+
+const pokemonList = document.getElementById('pokemonList');
+const loadMoreButton = document.getElementById('loadMoreButton');
+const pokemonListDetails = document.getElementById('pokemonListDetails');
+
+var modal = document.getElementById("myModal");
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+var pokemonDetailDiv = document.getElementById("pokemon-detail");
+
+span.onclick = function() {
+    modal.style.display = "none";
+  }
+  
+window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  }
+  
+function pokemonSelected (element,id) {
+    
+   console.log(element);
+   console.log(id);
+   
+}
+
+function loadPokemonItens(offset, limit) {
+   
+    pokeApi.getPokemons(offset,limit).then((pokemons = []) => {
+        const newHtml = pokemons.map((pokemon) =>  `
+            
+            <li class="pokemon ${pokemon.type}">
+            <div id="${pokemon.number}">
+            
+            <span class="number">#${pokemon.number}</span>
+            <span class="name">${pokemon.name}</span>
+            
+            <div class="detail">
+                <ol class="types">
+                    ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+                </ol>
+                <img src="${pokemon.photo}" alt="${pokemon.name}" />
+            </div>
+            <button id="buttonDetails" onclick='loadPokemonDetail(${pokemon.number}, ${1})' type="button">
+                Details
+            </button>
+            </li>
+            
+            </div>
+            `).join('');
+            
+        pokemonList.innerHTML += newHtml;
+        
+           
+    })
+    
+        
+    }
+
+
+ function loadPokemonDetail(id, limit) {
+    console.log(id);
+    console.log(limit);
+    pokeApi.getPokemon(id, limit).then((pokemons = []) => {
+        const newHtml = pokemons.map((pokemon) =>`
+        <li class="pokemonDetails ${pokemon.type}">
+
+        <span class="number">#${pokemon.number}</span>
+        <span class="name">${pokemon.name}</span>
+        
+        <div class="fullDetail">
+            <ol class="types">
+                ${pokemon.types.map((type) => `<li class="type ${type}">${type}</li>`).join('')}
+            </ol>
+            <img src="${pokemon.photo}" alt="${pokemon.name}" />
+        </div>
+
+        <div class="detailsBody">
+        
+        <label for="Altura">Altura</label>
+        <span class="${pokemon.type}">${pokemon.height}</span>
+        
+        <label for="Peso">Peso</label>
+        <span class="${pokemon.type}">${pokemon.weight}</span>
+        
+        </div>
+
+        <div class="detailsBodyData">
+        <label for="Habilidades">Habilidades</label>
+        <span><br>${pokemon.abilities}</span>
+        
+        </div>
+
+        
+        </li>
+        `).join('');
+        console.log(newHtml);
+        
+        pokemonDetailDiv.innerHTML = newHtml;     
+        modal.style.display = "block";   
+        
+    })
+}
+
+    
+   loadPokemonItens(execute());
+    loadPokemonDetail(id, limit);
+   
+   
+   function execute(){
+    const maxRecords = 151;
+    const limit = 10;
+    let offset = 0;
+
+    loadMoreButton.addEventListener('click', () => {
+        offset += limit;
+    
+        const qtdRecordsWithNextPage = offset + limit;
+    
+        if (qtdRecordsWithNextPage >= maxRecords) {
+         const newLimit = maxRecords - offset ;
+         loadPokemonItens(offset,newLimit);  
+         loadMoreButton.parentElement.removeChild(loadMoreButton);
+    
+        } else {
+        loadPokemonItens(offset,limit);
+        }
+    })
+    
+   }
+   
+   
---
+++ assets/js/pokemon-model.js
@@ -0,0 +1,24 @@
+class Pokemon {
+    number;
+    name;
+    types = [];
+    photo;
+}
+
+class PokemonData {
+    number;
+    name;
+    types = [];
+    photo;
+    height;
+    weight;
+    abilities = [];
+    baseExperience;
+    hp;
+    attack;
+    def;
+    sptattack;
+    spdef;
+    speed;
+} 
+
